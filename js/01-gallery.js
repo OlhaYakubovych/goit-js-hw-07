d774efa4830070946c921219d221283d7c72c312
OlhaYakubovych <yakcubovich.olga@gmail.com>
2022-05-03 18:14:14 +0200

    add task
    task 1,2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,61 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
 
-//------------------ Задача 1 --------------------------
+const galleryItemsEl = document.querySelector('.gallery');
+galleryItemsEl.innerHTML = addGalleryEl(galleryItems);
+galleryItemsEl.addEventListener('click', onImgModalOpen);
+
+let modalInstance;
+
+function addGalleryEl(items) {
+    return items
+        .map(({ preview, original, description }) => {
+            return `<div class = "gallery__item">
+<a class = "gallery__link" href = "${original}">
+    <img
+    class = "gallery__image"
+    src = "${preview}"
+    data-source = "${original}"
+    alt = "${description}"
+    />
+</a>
+</div>`;
+        }).join('');
+};
+
+function onImgModalOpen(event) {
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    event.preventDefault();
+
+    const originalUrl = event.target.dataset.source;
+
+    modalInstance = basicLightbox.create(
+        `<img src = "${originalUrl}">`,
+        {
+        onShow: (modalInstance) => {
+            addListener();
+        },
+        onClose: (modalInstance) => {
+            removeListener();
+        },
+    }
+
+    );
+    modalInstance.show();
+}
+
+function addListener() {
+    window.addEventListener('keydown', onEscClick);
+}
+function removeListener() {
+    window.removeEventListener('keydown', onEscClick);
+}
+function onEscClick(event) {
+    if (event.code === "Escape") {
+        modalInstance.close();
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+const galleryItemsEl = document.querySelector('.gallery');
+galleryItemsEl.innerHTML = addGalleryEl(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+
+function addGalleryEl(items) {
+    return items
+        .map(({ preview, original, description }) => {
+            return `<a class = "gallery__item" 
+            href = "${original}">
+    <img
+    class = "gallery__image"
+    src = "${preview}"
+    alt = "${description}"
+    />
+</a>`;
+        }).join('');
+};
+
